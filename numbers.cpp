9f6ca73353bbb5aa768187aa2e793eb1a5136692
jingjielim <jingjie3000@gmail.com>
2020-04-13 00:04:43 -0400

    Working with numbers

---
+++ numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // addition
+  cout << 40+5 << endl;
+  //modulo
+  cout << 10 % 3 << endl;
+  int wnum = 5;
+  double dnum = 5.5;
+  wnum++;
+  cout << ++wnum << endl;
+  // double and int returns double
+  cout << dnum + 5 << endl;
+  // int divide by int returns a integer
+  cout << 10 / 3 << endl;
+  
+  return 0;
+}
