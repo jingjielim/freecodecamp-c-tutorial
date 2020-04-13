552c7b9f78078a161604f7d5d3b228c95fbc0317
jingjielim <jingjie3000@gmail.com>
2020-04-12 22:42:42 -0400

    Add datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // character data type only takes one character
+    char grade = 'A';
+    // string data type takes a phrase
+    string phrase = "This is a string";
+    // integer
+    int age = 35;
+    // floating point
+    float height = 1.57;
+    // double precision
+    double gpa = 4.32;
+    // boolean
+    bool isMale = true;
+    
+    cout << gpa << endl;
+    
+    return 0;
+}
