51fba5113fbfb8abc954f32f8174a662fa0fce8b
jingjielim <jingjie3000@gmail.com>
2020-04-13 00:31:45 -0400

    Working with arrays

---
+++ arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int luckyNums[] = {4, 8, 15, 16, 23, 42};
+  cout << luckyNums[0] << endl;
+  luckyNums[0] = 55;
+  int randomNums[20];
+  cout << randomNums[0] << endl;
+  return 0;
+}
